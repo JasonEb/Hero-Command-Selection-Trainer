9cf04265ab5687a74f7963cae28a20ce01196df3
holyoke <jasonebueng@gmail.com>
2019-08-15 14:56:42 -0700

    introduces multiple  weighted choices

--- src/data/spells.js
+++ src/data/spells.js
@@ -1,44 +1,45 @@
  export const SpellsMap = {
-    "Acceleratle": 4.00366,
-    "Bang Family": 9.5348,
-    "Bounce": 4.39927,
-    "Flame Slash": 4.53114,
-    "Hatchet Man": 4.58974,
-    "Heal": 1.94505,
-    "Hocus Pocus": 1,
-    "Kaclang": 1.52747,
-    "Kacrackle Slash": 4.85714,
-    "Kamikazee": 1.55311,
-    "Magic Burst": 1.53114,
-    "Metal Slash": 2.07326,
-    "Oomph": 4.27839,
-    "Psyche Up": 4.22344,
-    "Sizz Family": 9.52381,
-    "Snooze": 4.24542,
-    "Whack Family": 5.67399,
-    "Zoom": 3.76923
+    "Acceleratle": { weight: 4.00366, active: true },
+    "Bang Family": { weight: 9.5348, active: true },
+    "Bounce": { weight: 4.39927, active: true },
+    "Flame Slash": { weight: 4.53114, active: true },
+    "Hatchet Man": { weight: 4.58974, active: true },
+    "Heal": { weight: 1.94505, active: true },
+    "Hocus Pocus": { weight: 1, active: true },
+    "Kaclang": { weight: 1.52747, active: true },
+    "Kacrackle Slash": { weight: 4.85714, active: true },
+    "Kamikazee": { weight: 1.55311, active: true },
+    "Magic Burst": { weight: 1.53114, active: true },
+    "Metal Slash": { weight: 2.07326, active: true },
+    "Oomph": { weight: 4.27839, active: true },
+    "Psyche Up": { weight: 4.22344, active: true },
+    "Sizz Family": { weight: 9.52381, active: true },
+    "Snooze": { weight: 4.24542, active: true },
+    "Whack Family": { weight: 5.67399, active: true },
+    "Zoom": { weight: 3.76923, active: true }
 }
 
-export const weightedSample = (map) => {
-    // Add up all the weights for all the items in the list
-    let totalWeight = Object.values(map).reduce((prev, curr) => prev + curr)
-
+export const weightedSample = (eligibleSpellsMap) => {
+    let chosenSpells = []
+    let totalWeight = 0
     // Pick a number at random between 1 and the sum of the weights
-    let rng = Math.floor(Math.random() * totalWeight) + 1 
+    // Iterate over the items and find rng less than current weight
+    while( chosenSpells.length <= 3 ) {
+        totalWeight = Object.values(eligibleSpellsMap).reduce((prev, curr) => prev + curr.weight, 0)
+        let rng = Math.floor(Math.random() * totalWeight) + 1 
 
-    // Iterate over the items
-    for (const spell in map) {
-        // For the current item, subtract the item’s weight from the random number that was originally picked
-
-        // Compare the result to zero. If less than or equal to zero then break otherwise keep iterating.
-        if (rng <= map[spell]){
-            return spell
+        for (const spellName in eligibleSpellsMap) {
+            let spell = eligibleSpellsMap[spellName]
+            if (rng <= spell.weight && spell.active) {
+                chosenSpells.push(spellName)
+                spell.active = false;
+                break;
+            }
+            rng -= spell.weight;
         }
-        rng -= map[spell];
     }
 
-    debugger
-    return "No spell how did this happen"
+    return chosenSpells;
 }
 
 export default SpellsMap;
